63c1b66cfe5ea27eb2c9dc5847cdb73c6940677e
Jim Palowski <jimmy@Jims-MacBook-Pro.local>
2017-12-12 01:56:36 -0800

    Modified

--- personal/apps.py
+++ personal/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class LoginConfig(AppConfig):
-    name = 'Login'
+class PersonalConfig(AppConfig):
+    name = 'personal'
